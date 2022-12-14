1d05b8952c7a71f8ed6f06da4db2a03f9775b176
KonstantinBerezin <berezinko90@gmail.com>
2022-12-14 17:34:43 +0500

    firebase set

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+const {
+  initializeAppCheck,
+  ReCaptchaV3Provider,
+} = require("firebase/app-check");
+
 const firebaseConfig = {
   apiKey: "AIzaSyDny6WCegcF4c4ZwYMX_6jLLVExTiuB7Lg",
   authDomain: "todo-list-c1818.firebaseapp.com",
@@ -11,6 +16,10 @@ const firebaseConfig = {
   appId: "1:626760406021:web:bb8db9ad45557d8fb0f86c",
   measurementId: "G-9ZL4W2EV3J",
 };
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider("6LfhF3wjAAAAAKOsLNm-j3pDOuIRFtFe6EAoyXvP"),
+  isTokenAutoRefreshEnabled: true,
+});
 
 const app = initializeApp(firebaseConfig);
 
